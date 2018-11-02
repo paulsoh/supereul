14c9895032670da8545bc4a6e52a9f83464440cb
yongseok <yongseok@linewalks.com>
2018-11-02 11:55:23 +0900

    Scaffolding for supereul pipy

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # supereul
 Minimalistic tensorflow session wrapper for ML experiments
+
+# runs on python > 3.6 and tensorflow > 1.12
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="supereul",
+    version="0.1",
+    description="Minimalistic Wrapper for TF graphs and hyperparameters",
+    url="https://github.com/linewalks/supereul",
+    author="Yongseok Soh",
+    author_email="yongseok@linewalks.com",
+)
